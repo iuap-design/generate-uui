f264ad8f3202990bc06b770be1c5df22db123d39
guoyff <guoyff@yonyou.com>
2016-06-02 15:54:49 +0800

    update

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -10,18 +10,39 @@ codeDirs=(
   "git@github.com:iuap-design/grid.git"
 )
 
-prodName=("iuap-design" "datatable" "datetimepicker" "tree" "grid")
+prodName=(
+  "iuap-design"
+  "datatable"
+  "datetimepicker"
+  "tree"
+  "grid"
+)
 
+# 检查项目目录是否
+Checkdir(){
+  if [ ! -d $1 ]
+  then
+    mkdir -p $1
+  fi
+}
+
+# 代码下载
 for dir in ${codeDirs[@]}
 do
-	git clone ${dir}
-	echo "${dir}项目代码下载完成~~!"
+  prodDir="$cdir/$name"
+
+  if [ ! -d $prodDir ]
+  then
+    git clone ${dir}
+  	echo "--- ${dir}项目代码下载完成 ---"
+  fi
 done
 
-for name in ${prodName}
+# 项目分支切换
+for name in ${prodName[@]}
 do
 	cd ${name}
   git checkout release
   cd ..
-  echo "${name}已经切换到releas分支"
+  echo "--- ${name}已经切换到releas分支 ---"
 done
