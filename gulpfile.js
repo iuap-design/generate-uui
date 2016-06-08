717750f580961d201a83f86f55cd575b1d7336e2
guoyff <guoyff@yonyou.com>
2016-06-08 15:18:30 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var minifycss = require('gulp-minify-css');
 
 var uuiPkg = require('./package.json');
 
@@ -51,6 +52,9 @@ gulp.task('js', function(){
 gulp.task('css', function(){
   gulp.src( pathOfCSS )
     .pipe(concat('u.css'))
+    .pipe(gulp.dest(uuiDist + '/css'))
+    .pipe(minifycss())
+    .pipe(rename('u.min.css'))
     .pipe(gulp.dest(uuiDist + '/css'))
 })
 
