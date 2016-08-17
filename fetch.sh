31d71698e88fcc0bfc3be6270f70f0aadea2a261
onvno <leewei2020@126.com>
2016-08-17 11:34:34 +0800

    add:更新初盘脚本，取消注释

--- fetch.sh
+++ fetch.sh
@@ -34,26 +34,26 @@ fi
 
 
 # 更新下载
-# for name in ${prodName[@]}
-# do
-#   prodDir="$cdir/$name"
-#   remoteDir="git@github.com:iuap-design/${name}.git"
+for name in ${prodName[@]}
+do
+  prodDir="$cdir/$name"
+  remoteDir="git@github.com:iuap-design/${name}.git"
 
-#   if [ ! -d $prodDir ]
-#   then
-#     echo "--- 开始下载${name} ---"
-#     git clone ${remoteDir}
-#   	echo "--- ${name}下载完成~~! ---"
-#   fi
-# done
+  if [ ! -d $prodDir ]
+  then
+    echo "--- 开始下载${name} ---"
+    git clone ${remoteDir}
+  	echo "--- ${name}下载完成~~! ---"
+  fi
+done
 
 # 分支切换到 release
-# for name in ${prodName[@]}
-# do
-# 	cd ${name}
-#   echo "--- ${name}进行分支切换和代码更新 ---"
-#   git checkout release
-#   git pull origin release
-#   cd ..
-#   echo "--- 分支切换和代码更新完成 ---"
-# done
+for name in ${prodName[@]}
+do
+	cd ${name}
+  echo "--- ${name}进行分支切换和代码更新 ---"
+  git checkout release
+  git pull origin release
+  cd ..
+  echo "--- 分支切换和代码更新完成 ---"
+done
